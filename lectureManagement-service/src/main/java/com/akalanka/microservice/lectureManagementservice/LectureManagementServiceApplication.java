9e209dce5c5f89c0ca9859295bf2359abe2c3d3e
akalanka <akalankamaduka95@gmail.com>
2018-05-14 19:24:27 +0530

    configuring eureka-naming-server

--- lectureManagement-service/src/main/java/com/akalanka/microservice/lectureManagementservice/LectureManagementServiceApplication.java
+++ lectureManagement-service/src/main/java/com/akalanka/microservice/lectureManagementservice/LectureManagementServiceApplication.java
@@ -2,8 +2,13 @@ package com.akalanka.microservice.lectureManagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RefreshScope
 public class LectureManagementServiceApplication {
 
 	public static void main(String[] args) {
